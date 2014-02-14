dbe7e231c9819e807ec8febeda3a3c366db2bdf7
Keith Veleba <velebak@ares.veleba.net>
2014-02-13 22:40:51 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var restify = require('restify');
+//var mongojs = require("mongojs");
+var twilio = require('twilio');
+
+var ip_addr = '127.0.0.1';
+var port    =  '8080';
+ 
+var server = restify.createServer({
+    name : "vocifero"
+});
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.CORS());
+
+function text_me(req,res,next){
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   var from_number = req.params['From'];
+   var message = req.params['Body'];
+   console.log(req.params);
+   var tw = twilio.TwimlResponse();
+   tw.message('Hello, Mobile Reader, texting me from ' + from_number + '. I have received your message of "' + message + '"');
+   //res.type('text/xml');
+   //res.contentType = 'text/xml';
+   //res.send(tw.toString());
+   var tml = tw.toString();
+   res.writeHead(200, {
+      'Content-Length': Buffer.byteLength(tml),
+	  'Content-Type' : 'text/xml'
+   });
+   res.write(tml);
+   res.end();
+}
+
+server.get({path : '/textme' , version : '0.0.1'} , text_me);
+
+
+server.listen(port ,ip_addr, function(){
+    console.log('%s listening at %s ', server.name , server.url);
+});
